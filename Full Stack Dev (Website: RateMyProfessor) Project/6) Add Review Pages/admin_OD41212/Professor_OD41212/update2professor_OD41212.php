33f1fbbfedbab39a8df2ca1690e1ae50ced48f15
Andy Lo <75705248+Eternaltime27@users.noreply.github.com>
2023-10-18 03:47:08 -0400

    Add files via upload

---
+++ Full Stack Dev (Website: RateMyProfessor) Project/6) Add Review Pages/admin_OD41212/Professor_OD41212/update2professor_OD41212.php
@@ -0,0 +1,29 @@
+<?php
+include 'member_OD41212.php';
+// Show MyGuests Data
+
+require 'dbcon_OD41212.php';
+
+// define variables and set to empty values
+$id = "";
+$id = $_GET['pid'];
+
+$pprofessor = $_POST['professor'];
+$pemail = $_POST['email'];
+$puniversity = $_POST['university'];
+$pcourse = $_POST['course'];
+
+$sql = "Update tblprofessor_OD41212 
+Set pname = '$pprofessor',
+pemail = $pemail,
+pcourse = '$pcourse',
+puniversity = '$puniversity',
+Where pid= '$id'";
+
+echo $sql;
+
+$conn->close();
+
+
+//echo "Data Deleted"
+//header('Location: displayprofessor_OD41212.php');
