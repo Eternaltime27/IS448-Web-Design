8d44e7e13448f2a4428cce9a1ff0a24b071a5ec6
Andy Lo <75705248+Eternaltime27@users.noreply.github.com>
2023-10-18 03:48:52 -0400

    Add files via upload

---
+++ Full Stack Dev (Website: RateMyProfessor) Project/7) Update Mechanic/homepage_OD41212.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+	<title> Rate MyProfessor - Template </title>
+	<link rel="stylesheet" href="style_OD41212/style_OD41212.css">
+	<link rel="stylesheet" href="style_OD41212/stylemenu_OD41212.css">
+
+</head>
+
+<body>
+
+<style>
+.hero-image {
+  background-image: url("bg_OD41212.png");
+  background-color: #cccccc;
+  height: 350px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
+
+
+	<header class="banner">
+	<img src="images_OD41212/bglogo_OD41212.svg">  
+
+		<div class="panel panel-nav">
+			<center>
+				<div class="dropdown">
+				<button class="dropbtn"><A href=homepage_OD41212.php ><B> HOME </B></A></button>
+				</div>
+				<div class="dropdown">
+				<button class="dropbtn"><A href="admin_OD41212/inde_OD41212.html" ><B> Login </B></A></button>
+				</div>
+				<div class="dropdown">
+				<button class="dropbtn"><A href=contactus_OD41212.php ><B> ContactUs </B></A></button>
+				</div>
+			</center>
+		</div>
+	
+	</header>
+	
+	<main>
+<?php
+
+				
+
+?>	
+				
+				<article>
+				<Center>
+				<div class="hero-image">
+					<div class="hero-text">
+					<image src="images_OD41212/bglogo_OD41212.svg" />
+					<p>Search professor</p>
+					<div>
+					<Form action="search_OD41212.php">
+					&nbsp;&nbsp;&nbsp;<input type="text" name="search" size="155" height="24">
+					<input type="image" src="images_OD41212/search_OD41212.png" alt="Submit" width="24" height="24">
+					</Form>
+				</div>	
+				</div>
+				</div>
+	
+				</article>
+				
+	</main>
+	
+	<footer>
+		<center>
+			Footer information
+		</center>
+	</footer>
+</body>
+</html>
